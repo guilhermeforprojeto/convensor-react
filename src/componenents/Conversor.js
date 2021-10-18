bcea0d56ea01c5bfdf4647518e170f650407e89b
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-10-18 17:13:53 -0300

    Configurando pra rojeto de converter moedas

---
+++ src/componenents/Conversor.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+export default class Conversor extends Component{
+
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="Conversor">
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <input type="text"></input>
+                <input type="button" value="Converter"></input>
+                <h2>Valor convertido:</h2>
+            </div>
+        )
+    }
+}
